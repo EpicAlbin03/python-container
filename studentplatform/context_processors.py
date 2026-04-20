f8f24a7ad0aab280635143d3ad569463c14edcce
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-04-20 13:33:08 +0200

    rm frontend

--- studentplatform/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpRequest
-
-from .models import Student
-
-
-def students_context(request: HttpRequest):
-    students = Student.objects.all()
-    return {'students_count': len(students)}
